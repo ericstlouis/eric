59ba5097f44aa833bccec0f1fa93dd125c8a8f6c
Eric St. Louis <ericstlouis2001@gmail.com>
2022-03-30 01:38:52 -0400

    nav => Nav, made in ./nav uppercase

--- components/index.js
+++ components/index.js
@@ -1,4 +1,4 @@
-export { default as Nav} from './nav';
+export { default as Nav} from './Nav';
 export { default as Head } from './Head';
 export { default as Work } from './Work';
 export { default as Footer } from './Footer';
